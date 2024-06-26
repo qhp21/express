29a2b03b7fddfd974df5484a1e1fcddd3749741f
hp21 <veber_22@bk.ru>
2024-06-26 10:27:21 +0300

    docker exam

---
+++ express-docker-app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Express started on port ${port}`);
+});
